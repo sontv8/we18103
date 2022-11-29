42881766af35b85f00c43993ff2ddcf7ce286330
sontv8 <sontv8@fpt.edu.vn>
2022-11-29 13:41:37 +0700

    cập nhật sản phẩm - giữ nguyên ảnh khi không chọn ảnh

--- controllers/updateProductController.php
+++ controllers/updateProductController.php
@@ -11,13 +11,19 @@
     $productId = $_POST["product-id"];
     $productName = $_POST["product-name"];
     $productDesc = $_POST["product-desc"];
-    $productImage =  $_FILES["new-image"]["name"];
     $productPrice =  $_POST["product-price"];
     $categoryId =  $_POST["category"];
 
+    if(empty($_FILES["new-image"]["name"])){ //kiểm tra xem có chọn ảnh hay không bằng cách check key "name" có trả về chuỗi rỗng hay không
+        $productImage = $_POST["old-image"];//nếu "name" trả về rỗng thì thực hiện lấy tên ảnh cũ từ $_POST theo key "old-image" để gán cho biến $productImage
+    }else{ //ngược lại nếu "name" không rỗng
+        $productImage =  $_FILES["new-image"]["name"]; //lấy ra tên ảnh từ file mới chọn và gán cho biến $productImage
+        move_uploaded_file($_FILES["new-image"]["tmp_name"],"../image/".$_FILES["new-image"]["name"]);//thực hiện chuyển file từ thư mục tạm vào thư mục image
+    }
+
     $query = "UPDATE products SET productName='$productName' , productDesc='$productDesc' , productImage='$productImage' , productPrice=$productPrice , categoryId=$categoryId WHERE id=$productId";
     connect($query);
-    move_uploaded_file($_FILES["new-image"]["tmp_name"],"../image/".$_FILES["new-image"]["name"]);
+    
 
     header("location: ../views/admin/product-management.php");
 ?>
